61682fc9d5ba4ee218a9fa0c51fc31797bde9f9d
akmal4896 <akmalboss007@email.arizona.edu>
2020-08-17 23:33:36 +0800

    Portfolio Web

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,112 +1,165 @@
-function makeWords() {
+const Portfolio = function() {
+	function makeWords() {
+		var words = [
+			{
+				text: "Stastical data analysis",
+				weight: 12.3
+			}, {
+				text: "R",
+				weight: 8
+			}, {
+				text: "Python",
+				weight: 14
+			}, {
+				text: "tech news",
+				weight: 3
+			}, {
+				text: "research",
+				weight: 7
+			}, {
+				text: "Machine Learning",
+				weight: 10
+			}, {
+				text: "Cloud services",
+				weight: 9
+			}, {
+				text: "Webapps",
+				weight: 15
+			}, {
+				text: "troubleshooting",
+				weight: 7
+			}
+		];
+		return words;
+	}
 
-	var words = [
-		{
-			text: "html5",
-			weight: 12.3
-		}, {
-			text: "css3",
-			weight: 12.5
-		}, {
-			text: "javascript",
-			weight: 13
-		}, {
-			text: "jquery",
-			weight: 12
-		}, {
-			text: "programming",
-			weight: 10
-		}, {
-			text: "python",
-			weight: 10
-		}, {
-			text: "java",
-			weight: 9
-		}, {
-			text: "nodejs",
-			weight: 11
-		}, {
-			text: "npm",
-			weight: 9
-		}, {
-			text: "web development",
-			weight: 10
-		}, {
-			text: "C",
-			weight: 8.3
-		}, {
-			text: "Scheme",
-			weight: 8.1
-		}, {
-			text: "php",
-			weight: 7.8
-		}, {
-			text: "hadoop",
-			weight: 8.5
-		}, {
-			text: "numpy",
-			weight: 7
-		}, {
-			text: "matplotlib",
-			weight: 7
-		}, {
-			text: "ruby",
-			weight: 8
-		}, {
-			text: "express",
-			weight: 9
-		}, {
-			text: "flask",
-			weight: 8.9
-		}, {
-			text: "responsive design",
-			weight: 10
-		}, {
-			text: "bootstrap",
-			weight: 10
-		}
-		/* ... */
-	];
-	return words;
-}
-
-function makeWordCloud(words) {
-	$('.teaching-domains').jQCloud(words, {delay: 120});
-}
-
-function displayWordCloud() {
-	var count = 1;
-	$(window).on('scroll', function() {
-		var y_scroll_pos = window.pageYOffset;
-		var scroll_pos_test = 2700; // set to whatever you want it to be
-		var words = makeWords();
-		if (y_scroll_pos > scroll_pos_test && count <= 1) {
-			makeWordCloud(words);
-			count++;
-		}
-	});
-}
-
-function designForm() {
-	$("#my-modal form").addClass("my-form");
-}
-
-$(document).ready(function() {
-	Typed.new("#writing-text", {
-		strings: [
-			"am a Full-Stack Web Developer.", "love everything about code.", "also teach programming to people.", "solve problems."
-		],
-		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
-		stringsElement: null,
-		// typing speed
-		typeSpeed: 1,
-		contentType: 'text',
-		callback: function() {
-			$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
-		},
-		preStringTyped: function() {},
-		onStringTyped: function() {}
+	$(function () {
+    	$('#btn1').click(function () {
+        	$('#datalist1 li:hidden').show();
+        	if ($('#datalist1 li').length == $('#datalist1 li:visible').length) {
+            	$('#btn1 ').hide();
+        	}
+    	});
 	});
 
-	displayWordCloud();
-})
+	$(function () {
+    	$('#btn2').click(function () {
+        	$('#datalist2 li:hidden').show();
+        	if ($('#datalist2 li').length == $('#datalist2 li:visible').length) {
+            	$('#btn2 ').hide();
+        	}
+    	});
+	});
+
+	$(function () {
+    	$('#btn3').click(function () {
+        	$('#datalist3 li:hidden').show();
+        	if ($('#datalist3 li').length == $('#datalist3 li:visible').length) {
+            	$('#btn3 ').hide();
+        	}
+    	});
+	});
+
+	$(function () {
+    	$('#btn8').click(function () {
+        	$('#datalist8 li:hidden').show();
+        	if ($('#datalist8 li').length == $('#datalist8 li:visible').length) {
+            	$('#btn8 ').hide();
+        	}
+    	});
+	});
+
+	$(function () {
+    	$('#btn4').click(function () {
+        	$('#datalist4 li:hidden').show();
+        	if ($('#datalist4 li').length == $('#datalist4 li:visible').length) {
+            	$('#btn4 ').hide();
+        	}
+    	});
+	});
+
+	$(function () {
+    	$('#btn5').click(function () {
+        	$('#datalist5 li:hidden').show();
+        	if ($('#datalist5 li').length == $('#datalist5 li:visible').length) {
+            	$('#btn5 ').hide();
+        	}
+    	});
+	});
+
+	$(function () {
+    	$('#btn6').click(function () {
+        	$('#datalist6 li:hidden').show();
+        	if ($('#datalist6 li').length == $('#datalist6 li:visible').length) {
+            	$('#btn6 ').hide();
+        	}
+    	});
+	});
+
+	$(function () {
+    	$('#btn7').click(function () {
+        	$('#datalist7 li:hidden').show();
+        	if ($('#datalist7 li').length == $('#datalist7 li:visible').length) {
+            	$('#btn7 ').hide();
+        	}
+    	});
+	});
+
+	$(function () {
+    	$('#btn9').click(function () {
+        	$('#datalist9 li:hidden').show();
+        	if ($('#datalist9 li').length == $('#datalist9 li:visible').length) {
+            	$('#btn9 ').hide();
+        	}
+    	});
+	});
+
+	function makeWordCloud(words) {
+		$('.teaching-domains').jQCloud(words, {delay: 120});
+	}
+
+	function displayWordCloud() {
+		var count = 1;
+		$(window).on('scroll', function() {
+			var y_scroll_pos = window.pageYOffset;
+			var scroll_pos_test = 2700; // set to whatever you want it to be
+			var words = makeWords();
+			if (y_scroll_pos > scroll_pos_test && count <= 1) {
+				makeWordCloud(words);
+				count++;
+			}
+		});
+	}
+
+	function designForm() {
+		$("#my-modal form").addClass("my-form");
+	}
+
+	function typeAnimation() {
+		Typed.new("#writing-text", {
+			strings: [
+				"am a Chemist.", "love research and data analysis.", "passionate with web programming.", "am a self-taught developer."
+			],
+			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+			stringsElement: null,
+			// typing speed
+			typeSpeed: 1,
+			contentType: 'text',
+			callback: function() {
+				$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
+			},
+			preStringTyped: function() {},
+			onStringTyped: function() {}
+		});
+	}
+
+	return {
+		displayWordCloud: displayWordCloud,
+		typeAnimation: typeAnimation
+	}
+
+}();
+
+
+Portfolio.displayWordCloud();
+Portfolio.typeAnimation();
